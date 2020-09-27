8a8849951e4d2dc2b62ac1265e6d3ede04daa0e6
陈黔 <chenq1@bsoft.com.cn>
2020-09-27 10:59:48 +0800

    test git.

--- src/main/java/com/bsoft/xnsmservice/XnsmserviceApplication.java
+++ src/main/java/com/bsoft/xnsmservice/XnsmserviceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class XnsmserviceApplication {
 
 	public static void main(String[] args) {
+//		test git.
 		SpringApplication.run(XnsmserviceApplication.class, args);
 	}
 
